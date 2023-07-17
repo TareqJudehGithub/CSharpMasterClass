7fbb3440f8c9111f229845379fbdfb41c313e362
TareqJudehGithub <tareq.joudeh@gmail.com>
2023-07-17 22:07:45 +0300

    Started task ToDo List. Completed Show All and Add Task methods.

---
+++ sec2_Fundamentals/Assignments/ToDoListTask/ToDoListTask/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ToDoListTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implement a ToDo List 
+            List<string> toDoList = new List<string>();
+
+            SelectToDoOption();
+            Console.WriteLine("\n");
+            AddToDo(toDoList);
+
+            List<string> allToDos = ShowAllToDos(toDoList);
+
+            foreach (string toDo in allToDos)
+            {
+                Console.WriteLine(toDo);
+            }
+
+            //Methods
+
+            // Implement a ToDo List - Select an option
+            void SelectToDoOption()
+            {
+                Console.WriteLine("" +
+                   "Hello,\n" +
+                   "What do you want to do?\n" +
+                   "[S]ee all TODOs\n" +
+                   "[A]dd a TODO\n" +
+                   "[R]emove a TODO\n" +
+                   "[E]xit\n");
+            }
+
+            // Implement a ToDo List See ALL ToDos
+            List<string> ShowAllToDos(List<string> toDo)
+            {
+                Console.WriteLine("\nToDo(s) for today are: ");
+                return toDo;
+            }
+
+            // Implement a ToDo List - Add method
+            void AddToDo(List<string> toDoList)
+            {
+                string description = string.Empty;
+
+                do
+                {
+                    Console.Write("Enter a TODO description: ");
+                    description = Console.ReadLine();
+
+                    toDoList.Add(description);
+                    if (description.Length > 0)
+                    {
+                        Console.WriteLine($"TODO successfully added: {description}");
+                    }
+
+                }
+                while (description.Length == 0);
+            }
+
+            // Implement a ToDo List - Remove method
+
+            //SelectOption(){
+            //    string choice = string.Empty;
+
+            //    switch (choice)
+            //    {
+            //        case "A":
+            //        case "a":
+
+            //    }
+            //}
+
+            // Console.ReadKey();
+        }
+    }
+}
